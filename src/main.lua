b55c25ee87687646d751596ce3ec7ecd5b26a81c
Francesco Paoletti <paoletti.francesco@gmail.com>
2025-12-17 22:05:37 +0100

    enter start mode with spacebar and toggle pause mode with escape key

--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,42 @@
 dofile("setup.lua")
 
+Modes = require("modes")
+
 print("+-------------------+")
 print("| HIT THE REFINERY! |")
 print("+-------------------+")
 print("coded with <3 by tanque")
+
+function love.load()
+	game_state = require("state")
+	print("GAME MODE: " .. game_state.mode)
+end
+
+function love.update(dt)
+	--
+end
+
+function love.draw()
+	--
+end
+
+function love.keypressed(key)
+	if key == "escape" then
+		if game_state.mode == Modes.START then
+			print("GAME MODE: " .. game_state.mode)
+		elseif game_state.mode == Modes.PAUSE then
+			game_state.mode = Modes.RUNNING
+			print("GAME MODE: " .. game_state.mode)
+		else
+			game_state.mode = Modes.PAUSE
+			print("GAME MODE: " .. game_state.mode)
+		end
+	end
+
+	if key == "space" then
+		if game_state.mode == Modes.START then
+			game_state.mode = Modes.RUNNING
+			print("GAME MODE: " .. game_state.mode)
+		end
+	end
+end

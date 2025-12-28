f18d26e1099105e815464fe6dc45583cb8d14350
Francesco Paoletti <paoletti.francesco@gmail.com>
2025-12-28 16:34:35 +0100

    button default option into its own properly scoped object

--- src/ui/button.lua
+++ src/ui/button.lua
@@ -3,21 +3,28 @@ require("utils.tables")
 local class = require("lib.middleclass")
 local Entity = require("core.entity")
 
-BUTTON_X = 0
-BUTTON_Y = 0
-BUTTON_WIDTH = 140
-BUTTON_HEIGHT = 50
-BUTTON_PADDING = 10
-BUTTON_MODE = "fill"
-BUTTON_BACKGROUND_COLOR = { love.math.colorFromBytes(255, 215, 0) }
-BUTTON_TITLE_COLOR = { love.math.colorFromBytes(0, 87, 183) }
-
 ---@class ButtonOptions: EntityOptions
 ---@field padding number
+---@field mode string
 ---@field background_color {r: number, g: number, b: number}
 ---@field title_color  {r: number, g: number, b: number}
----@field mode string
+---@field is_over boolean
+---@field hover_background_color {r: number, g: number, b: number}
+---@field hover_title_color  {r: number, g: number, b: number}
 ---@field text string
+local DEFAULT_BUTTON_OPTIONS = {
+	x = 0,
+	y = 0,
+	width = 140,
+	height = 50,
+	padding = 10,
+	mode = "fill",
+	background_color = { love.math.colorFromBytes(255, 215, 0) },
+	title_color = { love.math.colorFromBytes(0, 87, 183) },
+	is_over = false,
+	hover_background_color = { love.math.colorFromBytes(255, 215, 0) },
+	hover_title_color = { love.math.colorFromBytes(255, 215, 0) },
+}
 
 ---@class Button: Entity
 ---@field padding number
@@ -30,29 +37,26 @@ BUTTON_TITLE_COLOR = { love.math.colorFromBytes(0, 87, 183) }
 local Button = class("Button", Entity)
 
 function Button:initialize(opts)
-	options = merge_tables({
-		x = BUTTON_X,
-		y = BUTTON_Y,
-		width = BUTTON_WIDTH,
-		height = BUTTON_HEIGHT,
-		padding = BUTTON_PADDING,
-		background_color = BUTTON_BACKGROUND_COLOR,
-		title_color = BUTTON_TITLE_COLOR,
-		mode = BUTTON_MODE,
-		text = "BUTTON",
-	}, opts)
+	options = merge_tables(DEFAULT_BUTTON_OPTIONS, opts)
 	Entity.initialize(self, options)
 
 	self.padding = options.padding
 	self.background_color = options.background_color
 	self.title_color = options.title_color
+	self.is_hover = options.is_hover
+	self.hover_background_color = options.hover_background_color
+	self.hover_title_color = options.hover_title_color
 	self.mode = options.mode
 	self.text = options.text
 end
 
 function Button:draw()
 	-- draw the menu
-	love.graphics.setColor(self.background_color)
+	if self.is_hover then
+		love.graphics.setColor(self.hover_background_color)
+	else
+		love.graphics.setColor(self.background_color)
+	end
 	love.graphics.rectangle(self.mode, self.x, self.y, self.width, self.height)
 
 	-- draw the menu title
@@ -64,7 +68,13 @@ function Button:draw()
 	local title_height = font:getHeight(title)
 	title_x = self.x + ((self.width - title_length) / 2)
 	title_y = self.y + ((self.height - title_height) / 2)
-	love.graphics.setColor(self.title_color)
+	-- love.graphics.setColor(self.title_color)
+
+	if self.is_hover then
+		love.graphics.setColor(self.hover_title_color)
+	else
+		love.graphics.setColor(self.title_color)
+	end
 	love.graphics.print(title, title_x, title_y)
 end
 
@@ -74,13 +84,10 @@ function Button:update(dt)
 	local hot = mx > self.x and mx < self.x + self.width and my > self.y and my < self.y + self.height
 
 	if hot then
-		-- self.background_color = { love.math.colorFromBytes(0, 87, 183) }
-		self.background_color = { love.math.colorFromBytes(255, 215, 0) }
-		self.title_color = { love.math.colorFromBytes(255, 215, 0) }
+		self.is_hover = true
 		self.mode = "line"
 	else
-		self.background_color = BUTTON_BACKGROUND_COLOR
-		self.title_color = BUTTON_TITLE_COLOR
+		self.is_hover = false
 		self.mode = "fill"
 	end
 end
